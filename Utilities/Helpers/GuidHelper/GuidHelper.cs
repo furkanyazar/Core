fb1851c21feeb140d7a09fd16057e60549d0e57c
Furkan Yazar <frknyz1@outlook.com>
2021-10-30 00:45:32 +0300

    updated

---
+++ Utilities/Helpers/GuidHelper/GuidHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Core.Utilities.Helpers.GuidHelper
+{
+    public static class GuidHelper
+    {
+        public static string CreateGuid()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
