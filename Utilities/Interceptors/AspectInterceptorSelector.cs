3db514a32a46855762da1d153b51cd03c7e6e57b
Furkan Yazar <frknyz1@outlook.com>
2021-11-10 13:17:31 +0300

    cache, transaction and performance aspects added

--- Utilities/Interceptors/AspectInterceptorSelector.cs
+++ Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -1,4 +1,5 @@
 ﻿using Castle.DynamicProxy;
+using Core.Aspects.Autofac.Performance;
 using System;
 using System.Linq;
 using System.Reflection;
@@ -9,11 +10,10 @@ namespace Core.Utilities.Interceptors
     {
         public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
         {
-            var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>
-                (true).ToList();
-            var methodAttributes = type.GetMethod(method.Name)
-                .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
+            var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>(true).ToList();
+            var methodAttributes = type.GetMethod(method.Name).GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
             classAttributes.AddRange(methodAttributes);
+            classAttributes.Add(new PerformanceAspect(5));
 
             return classAttributes.OrderBy(x => x.Priority).ToArray();
         }
--- Utilities/IoC/ServiceTool.cs
+++ Utilities/IoC/ServiceTool.cs
@@ -10,6 +10,7 @@ namespace Core.Utilities.IoC
         public static IServiceCollection Create(IServiceCollection services)
         {
             ServiceProvider = services.BuildServiceProvider();
+
             return services;
         }
     }
